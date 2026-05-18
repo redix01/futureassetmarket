7f3876c16e693d700808cfd0bb4aa1eac7a1afcd
forgemaster <46707159+ebuka-odih@users.noreply.github.com>
2026-05-18 17:32:26 +0100

    Adjust loading brand text formatting

--- resources/views/pages/layout/app.blade.php
+++ resources/views/pages/layout/app.blade.php
@@ -87,6 +87,7 @@
                             <span data-text-preloader="S" class="letters-loading">S</span>
                             <span data-text-preloader="E" class="letters-loading">E</span>
                             <span data-text-preloader="T" class="letters-loading">T</span>
+                            <span data-text-preloader="-" class="letters-loading">-</span>
                             <br>
                             <span data-text-preloader="M" class="letters-loading">M</span>
                             <span data-text-preloader="A" class="letters-loading">A</span>
