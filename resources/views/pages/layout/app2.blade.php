c3b88c68c31af323146ff1694d22b8cfd39dec63
forgemaster <46707159+ebuka-odih@users.noreply.github.com>
2026-05-18 17:56:07 +0100

    Fix loading hyphen placement

--- resources/views/pages/layout/app2.blade.php
+++ resources/views/pages/layout/app2.blade.php
@@ -67,12 +67,12 @@
                             <span data-text-preloader="U" class="letters-loading">U</span>
                             <span data-text-preloader="R" class="letters-loading">R</span>
                             <span data-text-preloader="E" class="letters-loading">E</span>
+                            <span data-text-preloader="-" class="letters-loading">-</span>
                             <span data-text-preloader="A" class="letters-loading">A</span>
                             <span data-text-preloader="S" class="letters-loading">S</span>
                             <span data-text-preloader="S" class="letters-loading">S</span>
                             <span data-text-preloader="E" class="letters-loading">E</span>
                             <span data-text-preloader="T" class="letters-loading">T</span>
-                            <span data-text-preloader="-" class="letters-loading">-</span>
                             <br>
                             <span data-text-preloader="M" class="letters-loading">M</span>
                             <span data-text-preloader="A" class="letters-loading">A</span>
